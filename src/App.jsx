b4eb576b940e80dd342d71d25c7e258ea41f6bd3
krystleL21 <54385637+krystleL21@users.noreply.github.com>
2025-07-21 00:24:02 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import "./index.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
+
+function App() {
+
+
+  return (
+    <div className="app">
+<h1>React Weather App</h1>
+</div>
+  );
+}
+
+export default App
