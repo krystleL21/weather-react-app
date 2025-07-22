2372a93047bea95c393cae3b9295814c8d43e02c
krystleL21 <54385637+krystleL21@users.noreply.github.com>
2025-07-22 02:27:41 -0400

    Added API integration

--- src/Weather.jsx
+++ src/Weather.jsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './index.css';
 import './Weather.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import axios from 'axios';
 
 export default function Weather() {
+    
+    const [weatherData, setweatherData] = useState({ready:false})
+
+  
+function handleResponse(response) {
+    console.log(response.data);
+    setweatherData ({
+        ready: true,
+        temperature: response.data.main.temp,
+        humidity: response.data.main.humidity,
+        date: "Tuesday 02:15",
+        description: response.data.weather[0].description,
+        iconUrl: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
+        wind: response.data.wind.speed,
+        city: response.data.name,
+    })
+    
+    setReady(true);
+}
+if (weatherData.ready) {
     return(
         <div className="Weather">
             <form>
@@ -25,25 +46,33 @@ className="btn btn-primary w-100"/>
 </div>
 </div>
             </form>
-            <h1>Jamaica</h1>
+            <h1>{weatherData.city}</h1>
             <ul>
-                <li>Monday 07:31</li>
-                <li>Mostly cloudy</li>
+                <li>{weatherData.date}</li>
+                <li className="text-capitalize">{weatherData.description}</li>
             </ul>
             <div className="row">
                 <div className="col-6">
-                    <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" alt="MOstly cloudy" />
-                    <span className="temperature">80</span><span className="unit">°C</span>
+                    <img src={weatherData.iconUrl} alt={weatherData.description} />
+                    <span className="temperature">{Math.round(weatherData.temperature)}</span><span className="unit">°C</span>
                 </div>
                 <div className="col-6">
                     <ul>
-                        <li>Precipitation: 0%</li>
-                        <li>Humidy: 86%</li>
-                        <li>Wind: 2 mph</li>
+                        <li>Humidy: {weatherData.humidity}%</li>
+                        <li>Wind: {weatherData.wind} mph</li>
                     </ul>
                 </div>
             </div>
         
         </div>
     );
+
+} else {
+    let city = "Jamaica";
+    const apiKey = "Bf54175800a55e59e6c4d6461deeef12";
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+    
+    return ("Loading...");
+}
 }
